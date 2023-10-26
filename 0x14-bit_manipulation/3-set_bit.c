47936647b049fe7fc3ebcf8a9da6c0674260ebee
iintore <tonylewismanzi@gmail.com>
2023-10-26 03:29:25 -0700

    Binaries

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+#define CHAR_BITS 8 /*size of char*/
+#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
+
+void print_bin(unsigned long int number)
+{
+	if (number >> 1)
+		print_bin(number >> 1);
+
+	putc((number & 1) ? '1' : '0', stdout);
+}
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	#ifdef DEBUG
+	unsigned long int n_before = *n;
+	#endif
+
+	/*if index is greater than size of n in binary coded decimal*/
+	if (index > INT_BITS)
+		return (-1);
+
+	mask <<= index; /*create mask based on index position*/
+
+	*n = (*n | mask);
+
+	#ifdef DEBUG
+	printf("\n%ld in binary is ", n_before);
+	print_bin(n_before);
+	printf(" while mask right shifted to index %d is ", index);
+	print_bin(mask);
+	printf(" our new value n in binary is ");
+	print_bin(*n);
+	printf("\n\n");
+	#endif
+
+	return (1);
+}
